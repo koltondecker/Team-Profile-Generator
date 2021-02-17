e1e9f7a393aefa7416e094c8b7b7d77e3364da8f
Kolton Decker <deckerman25@gmail.com>
2021-02-16 19:16:42 -0600

    Built inquire propmt and answers that push constructor objects to an array.

--- index.js
+++ index.js
@@ -1,40 +1,110 @@
 const fs = require("fs");
 const inquire = require("inquirer");
 const Employee = require("./lib/Employee");
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const { exit } = require("process");
 
-function addEmployee() {
+const teamArray = [];
+
+function addManager() {
     inquire.prompt([
         {
             type: "input",
             name: "name",
-            message: "What is the name of the employee?"
+            message: "What is the name of the manager?"
         },
         {
             type: "input",
             name: "id",
-            message: "What is the ID of the employee?"
+            message: "What is the ID of the manager?"
         },
         {
             type: "input",
             name: "email",
-            message: "What is the email of the employee?"
+            message: "What is the email of the manager?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?"
         }
     ]).then( (answers) => {
 
-        const employee = new Employee(answers.name, answers.id, answers.email);
-        // console.log(employee);
-        console.log(employee.getName());
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamArray.push(manager);
+        addEmployee();
+
     })   
 }
 
-addEmployee();
+function addEmployee() {
+    inquire.prompt([
+        {
+            type: "list",
+            name: "nextEmployee",
+            message: "Would you like to add an Engineer, Intern, or are you finished with your team?",
+            choices: [
+                "Engineer",
+                "Intern",
+                "Done"
+            ]
+        },
+        {
+            when: (answers) => {
+                if(answers.nextEmployee === "Done") {
+                    process.exit();
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "name",
+            message: `What is the name of the employee?`
+        },
+        {
+            type: "input",
+            name: "id",
+            message: `What is the ID of the employee?`
+        },
+        {
+            type: "input",
+            name: "email",
+            message: `What is the email of the employee?`
+        },
+        {
+            when: (answers) => answers.nextEmployee === "Engineer",
+            type: "input",
+            name: "github",
+            message: `What is the github username for this Engineer?`
+        },
+        {
+            when: (answers) => answers.nextEmployee === "Intern",
+            type: "input",
+            name: "school",
+            message: `What is the name of the school this Intern attends?`
+        }
+    ]).then( (answers) => {
 
-// employee.getName();
-// const manager = new Manager();
-// Employee.getName();
+        console.log(answers);
+
+        if(answers.nextEmployee === "Engineer") {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            teamArray.push(engineer);
+        }
+        if(answers.nextEmployee === "Intern") {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+            teamArray.push(intern);
+        }
+        console.log(teamArray);
+        addEmployee();
+
+    })
+}
+
+addManager();
+
 // if(nextEmployee === "Engineer") {
 //     const engineer = new Engineer();
 // }
